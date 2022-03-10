5592c8b1c05b44d476c2b4df9ff562db5704451c
ad-jusk <adamj1215@gmail.com>
2022-03-10 12:52:06 +0100

    task 4 done

--- zad4/Main.java
+++ zad4/Main.java
@@ -1,3 +1,5 @@
+import java.io.File;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main{
@@ -13,6 +15,22 @@ public class Main{
         int op;
         Scanner scanner = new Scanner(System.in);
 
+        if(new File("moja_lista.txt").exists()){
+            System.out.println("Czy chcesz wczytac ostatnio utworzona liste? (1 - tak, 0 - nie)");
+            try{
+                op = scanner.nextInt();
+                scanner.nextLine();
+                if(op == 1){
+                    UserList.FillList("moja_lista.txt");
+                }
+            }
+            catch(InputMismatchException e){
+                System.out.println("Zla wartosc, robie pusta liste");
+                scanner.nextLine();
+            }
+        }
+        
+
         while(true){
             System.out.println("Co chcesz zrobic?");
             System.out.println("1 - dodaj do listy");
@@ -21,22 +39,30 @@ public class Main{
             System.out.println("4 - wyswietl liste");
             System.out.println("5 - zapisz liste");
             System.out.println("6 - zakoncz");
-
-            op = scanner.nextInt();
-            scanner.nextLine();
+            try{
+                op = scanner.nextInt();
+                scanner.nextLine();
+            }
+            catch(InputMismatchException e){
+                System.out.println("Zla wartosc!");
+                continue;
+            }
             if(op == 6){
                 break;
             }
 
             switch(op){
                 case 1:
-                    System.out.println("Podaj produkt:");
-                    product = scanner.nextLine();
-                    if(UserList.SearchForProduct(AvailableProducts, product)){
-                        System.out.println("Dodano produkt do listy");
-                    }
-                    else{
-                        System.out.println("Produkt nie jest dostepny");
+                    while(true){
+                        System.out.println("Podaj produkt:");
+                        product = scanner.nextLine();
+                        if(UserList.SearchForProduct(AvailableProducts, product)){
+                            System.out.println("Dodano produkt do listy");
+                            break;
+                        }
+                        else{
+                            System.out.println("Produkt nie jest dostepny");
+                        }
                     }
                     break;
                 case 2:
@@ -46,12 +72,17 @@ public class Main{
                     break;
                 case 3:
                     UserList.ClearList();
+                    System.out.println("Wyszyszczono liste");
                     break;
                 case 4:
                     UserList.PrintList();
                     break;
                 case 5:
-                    UserList.WriteListToFile("moja_lista.txt");
+                    if(UserList.WriteListToFile("moja_lista.txt") == 1){
+                        System.out.println("Blad zapisu do pliku");
+                        break;
+                    }
+                    System.out.println("Zapisano liste do pliku");
                     break;
                 default:
                     System.out.println("Zla wartosc!");
